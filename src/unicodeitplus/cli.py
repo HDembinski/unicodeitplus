13e544b8a17aee4900c21703dd6f787132d1afe5
Hans Dembinski <hans.dembinski@gmail.com>
2023-04-20 12:31:15 +0200

    better cli

--- src/unicodeitplus/cli.py
+++ src/unicodeitplus/cli.py
@@ -1,15 +1,21 @@
 """Console script for unicodeitplus."""
 import argparse
 import sys
-from unicodeitplus import replace
+from unicodeitplus import replace, parse
 
 
 def main() -> int:
-    """Console script for unicodeitplus."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("_", nargs="*")
+    """Convert simple LaTeX into an unicode approximation to paste anywhere."""
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument("ARG", nargs="+", help="some LaTeX code")
     args = parser.parse_args()
-    print(" ".join(replace(arg) for arg in args._))
+    s = ""
+    sargs = " ".join(args._)
+    if "$" not in sargs:
+        s = replace(sargs)
+    else:
+        s = parse(sargs)
+    print(s)
     return 0
 
 
