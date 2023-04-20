88428482a30479d2961c19c0fef61fa6a9c28183
Hans Dembinski <HDembinski@users.noreply.github.com>
2023-04-20 23:49:20 +0200

    fix wheel (#2)

--- src/unicodeitplus/parser.py
+++ src/unicodeitplus/parser.py
@@ -4,10 +4,28 @@ Parser for simple LaTeX.
 This parser supports only the simple subject of LaTeX that we typically use.
 """
 from lark import Lark
-from pathlib import Path
-from .transform import Transformer
+from .transform import ToUnicode
 
-with open(Path(__file__).parent / "latex.g") as f:
-    grammar = f.read()
+grammar = r"""
+start: (item | math)*
 
-parser = Lark(grammar, parser="lalr", transformer=Transformer())
+?atom: CHARACTER
+    | COMMAND
+
+?item: atom
+    | WS+
+    | group
+
+CHARACTER: /[^%#&\{\}^_]/ | ESCAPED
+ESCAPED: "\\\\" | "\\#" | "\\%" | "\\&"  | "\\{" | "\\}" | "\\_" | "\\,"
+group: "{" item* "}"
+math: "$" item* "$"
+SUBSCRIPT: "_"
+SUPERSCRIPT: "^"
+COMMAND: (("\\" WORD WS*) | SUBSCRIPT | SUPERSCRIPT)
+
+%import common.WS
+%import common.WORD
+"""
+
+parser = Lark(grammar, parser="lalr", transformer=ToUnicode())
