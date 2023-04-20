b72eb115f65c585b73f97cb13046d912295cf5d9
Hans Dembinski <hans.dembinski@gmail.com>
2023-04-20 23:46:07 +0200

    fix grammer

--- src/unicodeitplus/parser.py
+++ src/unicodeitplus/parser.py
@@ -4,10 +4,28 @@ Parser for simple LaTeX.
 This parser supports only the simple subject of LaTeX that we typically use.
 """
 from lark import Lark
-from pathlib import Path
 from .transform import Transformer
 
-with open(Path(__file__).parent / "latex.g") as f:
-    grammar = f.read()
+grammar = """
+start: (item | math)*
+
+?atom: CHARACTER
+    | COMMAND
+
+?item: atom
+    | WS+
+    | group
+
+CHARACTER: /[^%#&\{\}^_]/ | ESCAPED
+ESCAPED: "\\\\" | "\\#" | "\\%" | "\\&"  | "\\{" | "\\}" | "\\_" | "\\,"
+group: "{" item* "}"
+math: "$" item* "$"
+SUBSCRIPT: "_"
+SUPERSCRIPT: "^"
+COMMAND: (("\\" WORD WS*) | SUBSCRIPT | SUPERSCRIPT)
+
+%import common.WS
+%import common.WORD
+"""
 
 parser = Lark(grammar, parser="lalr", transformer=Transformer())
