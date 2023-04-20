a631900f63fd92b3811fd577379eb6099b9eddbc
Hans Dembinski <hans.dembinski@gmail.com>
2023-04-20 15:39:52 +0200

    shorter table

--- tools/generate_examples.py
+++ tools/generate_examples.py
@@ -2,53 +2,36 @@ from unicodeitplus import replace
 from tabulate import tabulate
 
 LATEX = r"""
-\alpha
-\beta
-\gamma
-\Gamma
-\infty
-e^+
-\mu^-
-\exists
-\in
-\int
-\sum
-\partial
+\alpha \beta \gamma \Gamma
+e^+ \mu^-
+\exists \in \int \sum \partial \infty
 \slash{\partial}
-\longrightarrow
-\to
-p\bar{p}
-\mathrm{t}\bar{\mathrm{t}}
-\mathcal{H}
-\mathbb{R}
-\underline{x}
-\phone
-\checkmark
-\dot{x}
-\ddot{x}
-\vec{x}
-A^6
-m_0
-\Im
-\Re
+\to \longrightarrow
+p\bar{p} \mathrm{t}\bar{\mathrm{t}}
+\mathcal{H} \mathbb{R}
+\phone \checkmark
+\underline{x} \dot{x} \ddot{x} \vec{x}
+A^6 m_0
+1.2 \times 10^{23}
+\Im \Re
 \hbar
-\perp
-\parallel
-\therefore
-\because
-\subset
-\supset
-p_T / \mathrm{GeV} c^{-1}
+\perp \parallel
+\therefore \because \subset \supset
+$p_T / \mathrm{GeV} c^{-1}$
 K^0_S
-D^{\ast\ast} \to hhee
-A \mathbf{x} \simeq \mathbf{b}
+$D^{\ast\ast} \to hhee$
+$A \cdot \mathbf{x} \simeq \mathbf{b}$
 """
 
 table = {"LaTeX": [], "Unicode": []}
 for latex in LATEX.split("\n"):
     if not latex or latex.isspace():
         continue
-    table["LaTeX"].append(f"``{latex}``")
-    table["Unicode"].append(f"``{replace(latex)}``")
+    table["LaTeX"].append(f"``{latex.strip('$')}``")
+    if latex.startswith("$"):
+        s = replace(latex.strip("$"))
+    else:
+        s = " ".join(replace(x) for x in latex.split())
+    table["Unicode"].append(f"``{s}``")
 
 print(tabulate(table, headers="keys", tablefmt="rst"))
