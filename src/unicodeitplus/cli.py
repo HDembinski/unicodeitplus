9cc525024bedab094799b4ef8e760548c1a407a2
Chase Meadors <c.ed.mead@gmail.com>
2023-07-02 11:49:38 -0600

    Add help for CLI args

--- src/unicodeitplus/cli.py
+++ src/unicodeitplus/cli.py
@@ -7,8 +7,8 @@ from unicodeitplus import UnicodeItPlus
 def main() -> int:
     """Convert simple LaTeX into an unicode approximation to paste anywhere."""
     parser = argparse.ArgumentParser(description=main.__doc__)
-    parser.add_argument("-r", "--preserve-roman", action="store_true")
-    parser.add_argument("-w", "--preserve-math-whitespace", action="store_true")
+    parser.add_argument("-r", "--preserve-roman", action="store_true", help="Do not italicize roman letters (A-Z a-z) in math")
+    parser.add_argument("-w", "--preserve-math-whitespace", action="store_true", help="Preserve whitespace between math characters")
     parser.add_argument("ARG", nargs="+", help="some LaTeX code")
     args = parser.parse_args()
     options_dict = vars(args)
